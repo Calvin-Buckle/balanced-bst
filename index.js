f28b1c73149011086cade2224a87967049af2dfb
Calvin Buckle <ccccbuckle@gmail.com>
2023-10-12 14:52:37 +0200

    create core parts of balanced binary search tree

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null
+        this.right = null
+    }
+}
+
+class Tree{
+    constructor(array){
+        this.root = this.buildtree(array);
+    }
+
+   buildtree(array){
+    if (array.length === 0) return null;
+    let mid = Math.floor(array.length /2);
+    let node = new Node(array[mid]);
+    node.left = this.buildtree(array.slice(0 , mid));
+    node.right = this.buildtree(array.slice(mid + 1));
+   return node;
+
+  }
+
+
+
+}
+
+
+
+
+
+let tree =new Tree([10,20,30,40,50,60,70,80,90])
+
+
+console.log(tree)
