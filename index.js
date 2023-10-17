f6664ece6bd99471b5bff1371b360e4081385e6f
Calvin Buckle <ccccbuckle@gmail.com>
2023-10-17 10:55:20 +0200

    create delete function

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ class Tree {
     }
     if (tmp.value === value) {
       return tmp;
-    }
+    } 
     if (tmp.value > value) {
       tmp.left = this.insert(value, tmp.left);
     } else {
@@ -35,8 +35,45 @@ class Tree {
     }
     return tmp;
   }
+
+
+
+ delete( value, tmp = this.root){
+  if (tmp === null){
+    return tmp
+  }
+
+  if(value < tmp.value){
+  tmp.left = this.delete(value, tmp.left)
+ }else if(value > tmp.value){
+  tmp.right = this.delete(value, tmp.right)
+ }else{
+
+  if(tmp.left === null)return tmp.right
+  else if (tmp.right === null )return tmp.left
+
+tmp.value = this.minValue(tmp.right);
+
+tmp.right =this.delete(tmp.value, tmp.right)
+
+}
+return tmp
+
+}
+
+minValue(node){
+  let minValue = node.value;
+  while(node.left != null){
+  minValue = node.left.value
+  node = node.left
+}
+return minValue
+ }
+
 }
 
 let tree = new Tree([10, 20, 30, 40, 50, 60, 70, 80, 90]);
+
 console.log(tree);
 console.log(tree.insert(35));
+console.log(tree.delete(50));
